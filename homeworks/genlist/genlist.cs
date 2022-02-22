86f69bfe6261a27edf6908c262601e3ad6fb18fc
Asger Brimnes Gardner <asgerbrimnes@gmail.com>
2022-02-22 22:47:22 +0100

    worked on genlist homework
    Signed-off-by: Asger Brimnes Gardner <asgerbrimnes@gmail.com>

--- homeworks/genlist/genlist.cs
+++ homeworks/genlist/genlist.cs
@@ -1,11 +1,23 @@
 public class genlist<T>{
 	public T[] data;
-	public int size {get{return data.Length;}}
-	public genlist() {data = new T[0];}
+	public int size=0;
+	public int capacity=8;	
+	public genlist() {data = new T[capacity];}
 	public void push(T item){ 
-		T[] newdata = new T[size+1];
-		for(int i=0;i<size;i++)newdata[i]=data[i];
-		newdata[size]=item;
-		data=newdata;
+		if(size==capacity){
+			T[] newdata = new T[capacity*=2];
+			for(int i=0;i<size;i++)newdata[i]=data[i];
+			data=newdata;
+		}
+		data[size]=item;
+		size++;
+		}
+	public void remove(int i){
+		for(int j=0;j<=size;j++){
+			if(j==i){
+				continue;	
+			}
+		
+		}
 	}
 }
