03849893cf9c9bf3157d0e9cb4415fd66768ebdb
Asger Brimnes Gardner <asgerbrimnes@gmail.com>
2022-02-26 16:01:53 +0100

    implemented gamma function in plots homework
    Signed-off-by: Asger Brimnes Gardner <asgerbrimnes@gmail.com>

--- homeworks/plots/B/functions.cs
+++ homeworks/plots/B/functions.cs
@@ -8,4 +8,12 @@ public static class functions{
 		double sum=t*(a[0]+t*(a[1]+t*(a[2]+t*(a[3]+t*a[4]))));
 		return 1-sum*Exp(-x*x);
 	}
+
+	public static double gamma(double x, bool ln){
+		double lngamma=x*Log(x+1/(12*x-1/x/10))-x+Log(2*PI/x)/2;
+                if(ln)return lngamma;
+		if(x<0)return PI/Sin(PI*x)/gamma(1-x, false);
+		if(x<9)return gamma(x+1, false)/x;
+		return Exp(lngamma);
+	}
 }
--- homeworks/plots/B/main.cs
+++ homeworks/plots/B/main.cs
@@ -3,8 +3,10 @@ using static System.Console;
 
 class main{
 	public static void Main(){
-		for(double x=-1;x<=1;x+=1.0/8){
-			WriteLine($"{x}	{functions.gamma(x)}");
+		for(double x=-5;x<=5;x+=1.0/8){
+			double gamma = functions.gamma(x, false);
+			double lngamma = functions.gamma(x, true);
+			WriteLine($"{x}	{gamma} {lngamma}");
 		}	
 	}
 }
