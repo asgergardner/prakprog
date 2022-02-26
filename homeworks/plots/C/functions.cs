ab6da39c217d94846d65d4f76b7480aa7dcb79d7
Asger Brimnes Gardner <asgerbrimnes@gmail.com>
2022-02-26 22:43:47 +0100

    played around with complex gamma
    Signed-off-by: Asger Brimnes Gardner <asgerbrimnes@gmail.com>

---
+++ homeworks/plots/C/functions.cs
@@ -0,0 +1,17 @@
+using static System.Math;
+
+public static class functions{
+	public static double erf(double x){
+		if(x<0) return -erf(-x);
+		double[] a={0.254829592,-0.284496736,1.421413741,-1.453152027,1.061405429};
+		double t=1/(1+0.3275911*x);
+		double sum=t*(a[0]+t*(a[1]+t*(a[2]+t*(a[3]+t*a[4]))));
+		return 1-sum*Exp(-x*x);
+	}
+
+	public static complex gamma(complex x){
+		if(x<0)return PI/Sin(PI*x)/gamma(1-x, false);
+		if(x<9)return gamma(x+1, false)/x;
+		return Exp(lngamma);
+	}
+}
