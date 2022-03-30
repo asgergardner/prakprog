fc596343afd54ceeacc7dfa14617dc5b8a439e7b
Asger <asgerbrimnes@gmail.com>
2022-03-30 11:04:32 +0200

    updated homework eigenvalues part B (again)
    Signed-off-by: Asger <asgerbrimnes@gmail.com>

--- homeworks/eigenvalues/B/main.cs
+++ homeworks/eigenvalues/B/main.cs
@@ -6,16 +6,18 @@ class main{
 	public static void Main(){
 		CheckRConvergence();
 		CheckNConvergence();
+		CompareSolutions();
 	}
 
-	public static void CheckRConvergence(){
-		int np = 100; 
-		int rmax = 20;
+	public static void CheckRConvergence(){ 
+		int rmax = 30;
+		double dr = 0.2; //Value taken from GenerateH method; could be written prettier I suppose
 		double[] table_energies = new double[] {-0.5, -0.125, -0.055};
 		using(var outfile = new System.IO.StreamWriter("r_convergence.txt")){
 			for(int energy=0; energy<=2; energy++){
-				for(double r=1; r<rmax; r+=0.2){ 
-					(matrix H, vector rs, double dr) = GenerateH(np, r);
+				for(double r=1; r<rmax; r+=0.5){
+					int np = (int)(r/(dr-0.1));	
+					(matrix H, vector rs, double dr_H) = GenerateH(np);
 					(vector e, matrix V) = evd.JacobiDiag(H);
                         		outfile.WriteLine($"{r} {e[energy]} {table_energies[energy]}");
 				}
@@ -31,7 +33,7 @@ class main{
 		using(var outfile = new System.IO.StreamWriter("n_convergence.txt")){
 			for(int energy=0; energy<=2; energy++){
 				for(int np=10; np<nmax; np+=5){
-					(matrix H, vector r, double dr) = GenerateH(np, rmax);
+					(matrix H, vector r, double dr) = GenerateH(np);
                         		(vector e, matrix V) = evd.JacobiDiag(H);
 					outfile.WriteLine($"{np} {e[energy]} {table_energies[energy]}");
                         	}
@@ -41,7 +43,7 @@ class main{
 	}
 
 	public static void CompareSolutions(){
-		(matrix H, vector r, double dr) = GenerateH(250, 50);
+		(matrix H, vector r, double dr) = GenerateH(250);
                 (vector e, matrix V) = evd.JacobiDiag(H);
 
                 var analyticalS = new Func<double,double>[] {x => 2*Exp(-x), x => -1.0/Sqrt(2)*(1-1.0/2*x)*Exp(-x/2), x => 2.0/(3*Sqrt(3))*(1-2.0/3*x+2.0/27*x*x)*Exp(-x/3)};
@@ -56,8 +58,8 @@ class main{
 
 	}
 
-	public static (matrix, vector, double) GenerateH(int npoints, double rmax){
-                double dr=rmax/(npoints+1);
+	public static (matrix, vector, double) GenerateH(int npoints){
+                double dr=0.2;
                 vector r = new vector(npoints);
                 for(int i=0;i<npoints;i++){
                         r[i]=dr*(i+1);
