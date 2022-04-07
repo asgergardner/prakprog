37518a9909a540e206b9f6fb3cf68c412c33207c
Asger <asgerbrimnes@gmail.com>
2022-04-07 12:00:21 +0200

    updated homework quadratures part C
    Signed-off-by: Asger <asgerbrimnes@gmail.com>

--- homeworks/quadratures/C/pyintegrate.py
+++ homeworks/quadratures/C/pyintegrate.py
@@ -10,10 +10,11 @@ def x_ex(x):
 def inv_xsq(x):
     return 1/(1+x*x)
 
-
-y1, err1, inf1 = quad(inv_xsq, 1, np.inf, full_output=True)
-y2, err2, inf2 = quad(x_ex, -np.inf, np.inf, full_output=True)
-y3, err3, inf3 = quad(inv_xsq, -np.inf, 0, full_output=True)
+eps = 0.001
+rel = 0.001
+y1, err1, inf1 = quad(inv_xsq, 1, np.inf, full_output=True, epsabs=eps, epsrel=rel)
+y2, err2, inf2 = quad(x_ex, -np.inf, np.inf, full_output=True, epsabs=eps, epsrel=rel)
+y3, err3, inf3 = quad(inv_xsq, -np.inf, 0, full_output=True, epsabs=eps, epsrel=rel)
 print("python 1/x^2:", y1, "error:", err1, "iterations:", inf1["neval"])
 print("python x_e^-x^2:", y2, "error:", err2, "iterations:", inf2["neval"])
-print("python 1/(1-x*x)", y3, "error:", err3, "iterations:", inf3["neval"])
+print("python 1/(1+x*x)", y3, "error:", err3, "iterations:", inf3["neval"])
