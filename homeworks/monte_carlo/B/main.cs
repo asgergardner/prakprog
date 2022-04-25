af9f20f608a729eed9785dcdc294d75d87f3448c
Asger <asgerbrimnes@gmail.com>
2022-04-25 10:35:06 +0200

    updated homework monte carlo part B
    Signed-off-by: Asger <asgerbrimnes@gmail.com>

--- homeworks/monte_carlo/B/main.cs
+++ homeworks/monte_carlo/B/main.cs
@@ -5,33 +5,18 @@ using System;
 
 public class main{
 	public static void Main(){
-		int NInit = 50000;
+		int NInit = 100000;
 		int NEnd = 1000000;
-		int NInterval = 50000;
+		int NInterval = 100000;
 		vector a0 = new double[2] {0,0};
 		vector b0 = new double[2] {1,1};
 		Func<vector,double> xy = vars => vars[0]*vars[1];
 		WriteLine("Integral of x*y from x=y=0 to x=y=1");
 		for(int N=NInit;N<=NEnd;N+=NInterval){
-			var res = quasimc(xy,a0,b0,N);
-			WriteLine($"res: {res.Item1} error: {res.Item2} points: {N}");
-		}
-		WriteLine("\n");
-
-		vector a1 = new double[2] {-1,-1};
-		vector b1 = new double[2] {1,1};
-		Func<vector,double> circle = delegate(vector vars){
-			double sqsq = Sqrt((vars[0]*vars[0]+vars[1]*vars[1])/PI);
-			if(sqsq>1){
-				return 0;
-			}else{
-				return sqsq;
-			}
-		};
-		WriteLine("Integral of r = sqrt(x^2+y^2) from x=y=-1 to x=y=1");
-		for(int N=NInit;N<=NEnd;N+=NInterval){
-			var res1 = quasimc(circle,a1,b1,N);
-			WriteLine($"res: {res1.Item1} error: {res1.Item2} points: {N}");
+			var res1 = quasimc(xy,a0,b0,N);
+			var res2 = plainmc(xy,a0,b0,N);
+			double table = 0.25;
+			WriteLine($"actual: {table} pseudo: {res2.Item1} quasi: {res1.Item1} error difference: {((res2.Item1-table) - (res1.Item1-table))} points: {N}");
 		}
 		WriteLine("\n");
 
@@ -40,8 +25,10 @@ public class main{
 		Func<vector,double> testf = vars => 1/((1 - Cos(vars[0])*Cos(vars[1])*Cos(vars[2]))*(PI*PI*PI));
 		WriteLine("Integral of (1-cos(x)*cos(y)*cos(z))^-1 from 0 to pi");
 		for(int N=NInit;N<=NEnd;N+=NInterval){
-                        var res2 = quasimc(testf,a2,b2,N);
-                        WriteLine($"res: {res2.Item1} error: {res2.Item2} points: {N}");
+                        var res3 = quasimc(testf,a2,b2,N);
+			var res4 = plainmc(testf,a2,b2,N);
+			double table = 1.3932039296856768591842462603255;
+			WriteLine($"actual: {table} pseudo: {res4.Item1} quasi: {res3.Item1} error difference: {((res4.Item1-table) - (res3.Item1-table))} points: {N}");
                 }
 	}
 }
--- homeworks/monte_carlo/B/mc.cs
+++ homeworks/monte_carlo/B/mc.cs
@@ -3,6 +3,29 @@ using static System.Math;
 using System;
 
 public static class mc{
+	public static (double, double) plainmc(Func<vector,double> intf, vector a, vector b, int N){
+                int n = a.size;
+                double V = 1;
+                for(int i=0; i<n; i++){
+                        V*=b[i]-a[i];
+                }
+                vector x = new vector(n);
+                double sum = 0;
+                double sqsum = 0;
+                var rand = new Random();
+                for(int i=0; i<N; i++){
+                        for(int j=0; j<n; j++){
+                                x[j]=a[j]+rand.NextDouble()*(b[j]-a[j]);
+                        }
+                        double fx = intf(x);
+                        sum += fx;
+                        sqsum += fx*fx;
+                }
+                double mean = sum/N;
+                double sigma = Sqrt(sqsum/N - mean*mean);
+                return(mean*V, sigma*V/Sqrt(N));
+        }
+
 	public static (double, double) quasimc(Func<vector,double> intf, vector a, vector b, int N){
 		int n = a.size;
 		double V = 1;
@@ -13,7 +36,7 @@ public static class mc{
 		Random rand = new Random();
 		double sum = 0;
 		double sqsum = 0;
-		for(int i=0; i<N; i++){
+		for(int i=1; i<N; i++){
 			vector quasiR = halton(i,n);
 			for(int j=0; j<n; j++){
 				x[j]=a[j]+quasiR[j]*(b[j]-a[j]);
